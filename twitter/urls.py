3c77083c28c03b510db0279b35a6df34438c7c8e
Your Name <you@example.com>
2022-01-18 23:42:58 -0800

    Create likes api

--- twitter/urls.py
+++ twitter/urls.py
@@ -18,9 +18,10 @@ from django.urls import path, include
 from rest_framework import routers
 from accounts.api import views
 from comments.api.views import CommentViewSet
-from tweets.api.views import TweetViewSet
+from likes.api.views import LikeViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
+from tweets.api.views import TweetViewSet
 
 import debug_toolbar
 
@@ -32,6 +33,7 @@ router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
+router.register(r'api/likes', LikeViewSet, basename='likes')
 
 urlpatterns = [
    path('admin/', admin.site.urls),

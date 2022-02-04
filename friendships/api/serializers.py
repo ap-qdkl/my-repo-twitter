804e70d284dbbdd316d03b637db34b82a77c30c8
Your Name <you@example.com>
2022-02-03 23:37:42 -0800

    friendships pagination

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -1,5 +1,6 @@
-from accounts.api.serializers import UserSerializer
+from accounts.api.serializers import UserSerializerForFriendship
 from friendships.models import Friendship
+from friendships.services import FriendshipService
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -14,16 +15,9 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
 
     def validate(self, attrs):
         if attrs['from_user_id'] == attrs['to_user_id']:
-            # You can't follow yourself
             raise ValidationError({
-                'message': 'from_user_id and to_user_id should be different.'
+                'message': 'from_user_id and to_user_id should be different'
             })
-        from django.contrib.auth.models import User
-        if not User.objects.filter(id=attrs['to_user_id']).exists():
-            raise ValidationError({
-                'message': 'You can not follow a non exist user.'
-            })
-
         return attrs
 
     def create(self, validated_data):
@@ -39,18 +33,35 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
 # 即 model_instance.xxx 来获得数据
 # https://www.django-rest-framework.org/api-guide/serializers/#specifying-fields-explicitly
 class FollowerSerializer(serializers.ModelSerializer):
-    user = UserSerializer(source='from_user')
+    user = UserSerializerForFriendship(source='from_user')
     created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
+        # 我们将在后序的课程中解决这个问题
+        return FriendshipService.has_followed(self.context['request'].user, obj.from_user)
 
 
 class FollowingSerializer(serializers.ModelSerializer):
-    user = UserSerializer(source='to_user')
+    user = UserSerializerForFriendship(source='to_user')
     created_at = serializers.DateTimeField()
+    has_followed = serializers.SerializerMethodField()
 
     class Meta:
         model = Friendship
-        fields = ('user', 'created_at')
+        fields = ('user', 'created_at', 'has_followed')
+
+    def get_has_followed(self, obj):
+        if self.context['request'].user.is_anonymous:
+            return False
+
+        # <TODO> 这个部分会对每个 object 都去执行一次 SQL 查询，速度会很慢，如何优化呢？
+        # 我们将在后序的课程中解决这个问题
+        return FriendshipService.has_followed(self.context['request'].user, obj.to_user)
